e0e897183b84aee7b7fe525d55ab59a3b18ae3fb
EvaldasP <72792658+EvaldasP@users.noreply.github.com>
2021-01-25 14:50:47 +0200

    Update cms_project.sql

--- cms_project.sql
+++ cms_project.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `cms_project` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `cms_project`;
 -- MySQL dump 10.13  Distrib 8.0.22, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: cms_project
@@ -49,4 +51,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-01-25 14:26:43
+-- Dump completed on 2021-01-25 14:50:21
